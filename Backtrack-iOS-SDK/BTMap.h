6e9734ad3517b9748809a4679c7f8c70debfaf5c
Ahmet Ozisik <ozisikahmet@gmail.com>
2015-04-28 14:59:01 +0300

    added BTMap class

---
+++ Backtrack-iOS-SDK/BTMap.h
@@ -0,0 +1,13 @@
+//
+//  BTMap.h
+//  Pods
+//
+//  Created by Ahmet Özışık on 28.04.2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTMap : NSObject
+
+@end
---
+++ Backtrack-iOS-SDK/BTMap.m
@@ -0,0 +1,13 @@
+//
+//  BTMap.m
+//  Pods
+//
+//  Created by Ahmet Özışık on 28.04.2015.
+//
+//
+
+#import "BTMap.h"
+
+@implementation BTMap
+
+@end
